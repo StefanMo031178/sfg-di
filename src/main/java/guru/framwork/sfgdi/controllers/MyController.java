d5704a7e17703a57c46ee4d62b80043f4677bd11
StefanMo <61204035+StefanMo031178@users.noreply.github.com>
2020-04-13 12:07:57 +0200

    @Primary
    Property
    Setter
    Constructor

--- src/main/java/guru/framwork/sfgdi/controllers/MyController.java
+++ src/main/java/guru/framwork/sfgdi/controllers/MyController.java
@@ -1,13 +1,19 @@
 package guru.framwork.sfgdi.controllers;
 
+import guru.framwork.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("Hello World!!!");
-        return "Hi Folks!!!";
+        return greetingService.sayGreeting();
     }
 
 }
