638d9d235ba2c2faa379a34d97ad54ebf49177b3
StefanMo <61204035+StefanMo031178@users.noreply.github.com>
2020-04-13 11:42:19 +0200

    @ Anotationen
    Property
    Setter
    Constructor

--- src/main/java/guru/framwork/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/framwork/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,10 @@
 package guru.framwork.sfgdi.controllers;
 
 import guru.framwork.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
